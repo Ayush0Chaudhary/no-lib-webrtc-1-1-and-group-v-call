5d8f90ffa503f6212d151394644937603e1b22f5
Ayush0Chaudhary <ayush0000ayush@gmail.com>
2024-03-19 12:25:58 +0530

    chore: Made the Website

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,23 @@
-import { Button } from "@/components/ui/button";
+import Navbar from '@/components/nav';
+import { HoverEffect } from '@/components/ui/hover-card';
+import { Separator } from '@/components/ui/separator';
+import { projects } from '@/lib/const';
+import React from 'react';
 
-const Home = () => {
-  return <Button>Click ME</Button>;
+
+
+const Homepage = () => {
+  return (
+    <div className='bg-black min-h-screen p-8'>
+      <div className='container mx-auto'>
+        <Navbar />
+        <Separator />
+        <div className='max-w-5xl mx-auto px-8'>
+          <HoverEffect items={projects} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export default Homepage;
